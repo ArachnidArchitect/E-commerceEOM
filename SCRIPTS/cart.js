64e0ad12f48100246e3199a74e20f256a8d64b27
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-12 17:55:04 +0200

    cart 5% done

--- SCRIPTS/cart.js
+++ SCRIPTS/cart.js
@@ -1,14 +1,24 @@
-let profile = document.querySelector('.navEnd img')
-let profileMenu = document.querySelector('#navAccList')
 
 
-profileMenu.addEventListener('mouseleave', () => {
-    profileMenu.style.display = 'none'
 
 
-})
-profile.addEventListener('mouseover', () => {
-    profileMenu.style.display = 'block'
 
 
-})
+
+
+
+// Js for profile options
+    let profile = document.querySelector('.navEnd img')
+    let profileMenu = document.querySelector('#navAccList')
+
+
+    profileMenu.addEventListener('mouseleave', () => {
+        profileMenu.style.display = 'none'
+
+
+    })
+    profile.addEventListener('mouseover', () => {
+        profileMenu.style.display = 'block'
+
+
+    })
