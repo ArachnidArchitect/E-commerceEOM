830716089bdbdb59eb3e209e4ac1f9dd4e727895
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-13 16:47:53 +0200

    about transition is broken. fixed nav bar as well

---
+++ SCRIPTS/about.js
@@ -0,0 +1,15 @@
+// Js for profile options
+let profile = document.querySelector('.navEnd img')
+let profileMenu = document.querySelector('#navAccList')
+
+
+profileMenu.addEventListener('mouseleave', () => {
+    profileMenu.style.display = 'none'
+
+
+})
+profile.addEventListener('mouseover', () => {
+    profileMenu.style.display = 'block'
+
+
+})
